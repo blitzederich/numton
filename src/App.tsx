99984d7e19b1b73ff2f61a21ae74a02073e6355c
Alexander Samorodov <blitzederich@gmail.com>
2024-12-11 20:37:05 +0300

    some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import cn from 'classnames';
 
 import './App.css'
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0);
   const [animateEnabled, setAnimateEnabled] = useState(false);
+  const [timeoutID, setTimeoutID] = useState<ReturnType<typeof setTimeout>>();
 
   const increase = () => {
     setCount(prev => prev + 1);
     setAnimateEnabled(true);
-    setTimeout(() => setAnimateEnabled(false), 200);
+    if (timeoutID) {
+      clearTimeout(timeoutID);
+    }
+    setTimeoutID(setTimeout(() => setAnimateEnabled(false), 100));
   }
 
   useEffect(() => {
-    const interval = setInterval(increase, 1000);
-    return () => clearInterval(interval);
+    if (window.Telegram) {
+      const tg = window.Telegram.WebApp;
+      tg.expand();
+
+      setTimeout(() => {
+        const user = tg.initDataUnsafe.user;
+        if (!user) {
+          return;
+        }
+
+        tg.showAlert(`Hello, ${user.first_name}!`);
+
+        tg.requestContact((ok, res) => {
+          if (ok) {
+            tg.showAlert(JSON.stringify(res));
+          }
+        });
+      });
+    }
   }, []);
 
   return (
-    <div className={cn('counter-container', animateEnabled && 'animate')}>
-      {count}
-    </div>
+    <Fragment>
+      <div className={cn('counter-container', animateEnabled && 'animate')}>
+        {count}
+      </div>
+      <div className="button-container">
+        <button className="button" onClick={increase}>
+          {/* <span style={{width: '66.6%', height: '100%', backgroundColor: '#fff', display: 'block'}} /> */}
+          <span className="button-text">UP</span>
+        </button>
+      </div>
+    </Fragment>
   )
 }
-
-export default App
